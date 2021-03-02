50912be9160ad1f464c0e3603050ce52918b2b21
amin-ghanmi <76432419+amin-ghanmi@users.noreply.github.com>
2021-03-02 14:37:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -23,5 +23,5 @@ app.use(express.static("public"));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, (err) => err ?
     console.error(err) :
-    console.log('the server is running...')
+    console.log(`the server is running on port: ${PORT}...`)
 );
